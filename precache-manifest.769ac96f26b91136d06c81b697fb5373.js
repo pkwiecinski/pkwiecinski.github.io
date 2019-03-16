782a3d9db890711834186d9bae5611b01c8b0501
Paweł Kwieciński <pa.kwiecinski96@gmail.com>
2019-03-16 14:47:57 +0100

    my first application in react

---
+++ precache-manifest.769ac96f26b91136d06c81b697fb5373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ced61118cddfe759527",
+    "url": "/static/js/main.1adc1ded.chunk.js"
+  },
+  {
+    "revision": "e92f4f9f309e26813d86",
+    "url": "/static/js/2.aea87b46.chunk.js"
+  },
+  {
+    "revision": "6ced61118cddfe759527",
+    "url": "/static/css/main.d6ac79be.chunk.css"
+  },
+  {
+    "revision": "e92f4f9f309e26813d86",
+    "url": "/static/css/2.d5ef6d65.chunk.css"
+  },
+  {
+    "revision": "efd3488033f95c4fbc6d512c92f6b93b",
+    "url": "/index.html"
+  }
+];
